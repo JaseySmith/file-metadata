6b08ae92c15b2f6b8d7dc5a90ed179f1c23e9878
JaseySmith <jaseyhsmith@gmail.com>
2023-07-20 01:02:40 -0700

    Created route to handle file upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var express = require('express');
+const express = require("express");
 var cors = require('cors');
 require('dotenv').config()
-
-var app = express();
+const multer = require("multer");
+const app = express();
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,10 +12,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Serve the HTML and CSS files from the public folder
+app.use(express.static("public"));
 
+// POST route to handle the file upload
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
 
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
